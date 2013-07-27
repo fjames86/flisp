bd3b416d2ffc180d39dfbdb58eb7667d4be837f5
Frank James <frank.a.james@gmail.com>
2013-07-28 00:09:34 +0100

    Adding core lisp files

---
+++ lisp/core.lisp
@@ -0,0 +1,31 @@
+
+
+(define (1+ x) (+ x 1))
+
+(define (sq x) (* x x))
+
+(define (caar x) (car (car x)))
+(define (cadr x) (car (cdr x)))
+(define (cdar x) (cdr (car x)))
+(define (cddr x) (cdr (cdr x)))
+(define (caaar x) (car (caar x)))
+(define (caadr x) (car (cadr x)))
+(define (cadar x) (car (cdar x)))
+(define (caddr x) (car (cddr x)))
+(define (cdaar x) (cdr (caar x)))
+(define (cdadr x) (cdr (cadr x)))
+(define (cddar x) (cdr (cdar x)))
+(define (cdddr x) (cdr (cddr x)))
+
+
+(define (mapcar proc list)
+	(if list
+		(cons (proc (car list))
+			  (mapcar proc (cdr list)))))
+
+(define (mapcdr proc list)
+	(if list
+		(cons (proc list)
+			  (mapcdr proc (cdr list)))))
+
+
