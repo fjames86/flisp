d477df75530c85353aba3e600aa418b7598e6304
Frank James <frank.a.james@gmail.com>
2013-07-28 13:13:20 +0100

    adding errors

--- src/error.c
+++ src/error.c
@@ -10,13 +10,13 @@ void error (char *message, char *location) {
 	if (message != NULL) {
 		e->message = gc_new_string(message);
 	} else {
-		e->message = NULL;
+		e->message = gc_new_string("");
 	}
 
 	if (location != NULL) {
 		e->location = gc_new_string(location);
 	} else {
-		e->location = NULL;
+		e->location = gc_new_string("");
 	}
 	
 	e->next = error_p;
