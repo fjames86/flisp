2ba74129f5717ed57af0d742f8f2170593ef735d
Frank James <frank.a.james@gmail.com>
2013-08-07 10:08:35 +0100

    reorganising files

---
+++ kernel/make-floppy.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# make the flat image
+echo "Creating blank floppy image ..."
+dd if=/dev/zero of=floppy.img bs=512 count=2880
+
+# set up the loop back device
+losetup /dev/loop0 floppy.img
+
+# create the file system
+echo "Creating file system ..."
+mkfs -t ext2 /dev/loop0
+
+if [ ! -d looptmp ]; then
+    mkdir looptmp
+fi
+
+# mount 
+echo "Mounting ... "
+mount -t ext2 /dev/loop0 looptmp
+
+echo "Copying in files ... "
+# add directories
+mkdir looptmp/boot
+mkdir looptmp/boot/grub
+
+# copy files
+cp /usr/lib/grub/i386-pc/stage1 looptmp/boot/grub
+cp /usr/lib/grub/i386-pc/stage2 looptmp/boot/grub
+
+echo "(fd0) /dev/loop0" > looptmp/boot/grub/device.map
+
+echo "Installing grub ... "
+
+echo "root (fd0)" > grub-tmp
+echo "setup (fd0)" >> grub-tmp
+grub --device-map=looptmp/boot/grub/device.map /dev/loop0 < grub-tmp
+rm -f grub-tmp
+
+
+
+cp menu.lst looptmp/boot/grub
+cp kernel.bin looptmp/boot/grub
+
+echo "Cleaning up ..."
+umount looptmp
+losetup -d /dev/loop0
+
+
+
+
